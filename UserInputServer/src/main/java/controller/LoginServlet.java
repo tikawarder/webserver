0996947d4870e90e9e5c64fe5613cfa53e0add1d
Tamas Biro <tamas.biro@liferay.com>
2025-11-21 17:48:28 +0100

    Develop login pages for testing the broken access control vulnerability

---
+++ UserInputServer/src/main/java/controller/LoginServlet.java
@@ -0,0 +1,38 @@
+package controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+    private static final String VALID_USERNAME = "user";
+    private static final String VALID_PASSWORD = "password";
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String username = request.getParameter("username");
+        String password = request.getParameter("pwd");
+
+        if (isValidCredentials(username, password)) {
+            HttpSession session = request.getSession();
+
+            session.setAttribute("loggedInUser", username);
+
+            response.sendRedirect(request.getContextPath() + "/search.jsp");
+        } else {
+            request.setAttribute("errorMessage", "Wrong username or password");
+            request.getRequestDispatcher("/login.jsp").forward(request, response);
+        }
+    }
+
+    private boolean isValidCredentials(String username, String password) {
+        return VALID_USERNAME.equals(username) && VALID_PASSWORD.equals(password);
+    }
+}
