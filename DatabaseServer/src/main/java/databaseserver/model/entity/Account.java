8362de78dfcaecea69bc024cc508f1edd8102e21
Tamas Biro <tamas.biro@liferay.com>
2026-01-14 15:42:44 +0100

    Unit & Integration Testing: write the integration test

--- DatabaseServer/src/main/java/databaseserver/model/entity/Account.java
+++ DatabaseServer/src/main/java/databaseserver/model/entity/Account.java
@@ -3,6 +3,7 @@ package databaseserver.model.entity;
 import databaseserver.model.Role;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.*;
 
@@ -28,7 +29,7 @@ public class Account {
     @Column(nullable = false)
     private String password;
 
-    @NotBlank(message = "role can not be empty")
+    @NotNull
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private Role role;
