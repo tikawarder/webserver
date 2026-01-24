e269b77932826d0c0ed44444cdb4347b5d5829bd
Tamas Biro <tamas.biro@liferay.com>
2026-01-24 17:35:55 +0100

    Implement dev properties and use secret file (without pushing to git repository)

--- DatabaseServer/src/main/java/databaseserver/controller/AuthController.java
+++ DatabaseServer/src/main/java/databaseserver/controller/AuthController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin(origins = {"https://frontend-react-801953368913.us-east1.run.app", "http://localhost:3000", "http://localhost:8080"}, allowCredentials = "true")
+@CrossOrigin(origins = {"https://frontend-react-801953368913.us-east1.run.app", "https://dev.birotamas.hu", "http://localhost:3000", "http://localhost:8080"}, allowCredentials = "true")
 public class AuthController {
 
     @Autowired
